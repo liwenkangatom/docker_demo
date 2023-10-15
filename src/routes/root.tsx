44c3b62781b2fb1c61b0cfbd4914bd422f35d3cf
willadam <liwenkang4757@outlook.com>
2023-10-15 16:10:55 +0800

    feat: change web name

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -7,8 +7,8 @@ export default function Root() {
         <nav
           className='max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between'
           aria-label='Global'>
-          <a className='flex-none text-xl font-semibold dark:text-white' href='http://localhost'>
-            Demo
+          <a className='flex-none text-xl font-semibold dark:text-white' href='/'>
+            NFSBT
           </a>
           <div className='flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:pl-5'>
             <Link
